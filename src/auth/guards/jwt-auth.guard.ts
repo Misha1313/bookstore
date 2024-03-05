ff5a983d4dd8f6c7b6129cbc107cb3a2dd008d21
Mikheil Marghishvili <m.marghishvili@adjarabet.com>
2024-03-05 15:03:25 +0400

    push test

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -3,12 +3,12 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
-    canActivate(context: ExecutionContext) {
-        const request = context.switchToHttp().getRequest<Request>();
-        
-        // console.log('context', request);
-        // Add your custom authentication logic here
-        // for example, call super.logIn(request) to establish a session.
-        return super.canActivate(context);
-    }
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    console.log('context', request);
+    // Add your custom authentication logic here
+    // for example, call super.logIn(request) to establish a session.
+    return super.canActivate(context);
+  }
 }
